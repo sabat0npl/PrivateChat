c09439a3f4c1a329e0e71aa4bdff646e29b541c6
Bruno Zablotny <rusholdfield@gmail.com>
2021-07-26 22:17:55 +0200

    Added basic chat functionalities

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Platform, StyleSheet, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = (props) => {
   const styles = StyleSheet.create({
@@ -9,7 +10,57 @@ const Chat = (props) => {
     },
   });
 
-  return <View style={styles.container}></View>;
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "This is a system message",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  const onSend = (messages = []) => {
+    setMessages((previousState) => GiftedChat.append(previousState, messages));
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{ right: { backgroundColor: "#000" } }}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <GiftedChat
+        renderBubble={renderBubble}
+        messages={messages}
+        onSend={onSend}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </View>
+  );
 };
 
 export default Chat;
